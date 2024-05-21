60b58f052dadfcfb6edcc2cfc389721d95f79464
VictorCyuvi20 <victoramancio2020@gmail.com>
2024-05-21 17:04:03 -0300

    ATUALIZAÇÃO FODA

--- MB.py
+++ MB.py
@@ -1,33 +1,29 @@
 import pygame
-
-class Personagem:
-
+import random
+class Obstaculo1:
+    
     def __init__(self,arquivo_imagem,largura_imagem,altura_imagem,x_inicial,y_inicial):
         self.imagem = pygame.image.load(arquivo_imagem)
-
+        
         self.largura = largura_imagem
         self.altura = altura_imagem
-
+        
         self.imagem = pygame.transform.scale(self.imagem,(self.largura,self.altura))
-
+        
         self.pos_x = x_inicial
         self.pos_y = y_inicial
 
+        self.velocidade = random.randint(1,10)
+    
+        
         self.mascara = pygame.mask.from_surface(self.imagem)
 
-        self.pontuacao = 0
-
-
-    def desenhar(self, tela):
+    def apareca(self, tela):
         tela.blit(self.imagem,(self.pos_x,self.pos_y))
 
-    def movimenta_via_controle(self,direita,esquerda):
-        teclas = pygame.key.get_pressed()
-        
-        if teclas[direita]:
-            if self.pos_x < 800 - self.largura:
-                self.pos_x = self.pos_x + 5
-
-        if teclas[esquerda]:
-            if self.pos_x > 0:
-                self.pos_x = self.pos_x - 5
+    def movimenta(self):
+        self.pos_y = self.pos_y + self.velocidade
+        if self.pos_y > 900:
+            self.pos_y = 1
+            self.velocidade = random.randint(1,10)
+            self.pos_x = random.randint(0,800)
--- gp.py
+++ gp.py
@@ -1,33 +1,29 @@
 import pygame
-
-class Personagem:
-
+import random
+class Griffith:
+    
     def __init__(self,arquivo_imagem,largura_imagem,altura_imagem,x_inicial,y_inicial):
         self.imagem = pygame.image.load(arquivo_imagem)
-
+        
         self.largura = largura_imagem
         self.altura = altura_imagem
-
+        
         self.imagem = pygame.transform.scale(self.imagem,(self.largura,self.altura))
-
+        
         self.pos_x = x_inicial
         self.pos_y = y_inicial
 
+        self.velocidade = random.randint(5,10)
+        
+        self.posicao = random.randint(20,780)
+        
         self.mascara = pygame.mask.from_surface(self.imagem)
 
-        self.pontuacao = 0
-
-
-    def desenhar(self, tela):
+    def apareca(self, tela):
         tela.blit(self.imagem,(self.pos_x,self.pos_y))
 
-    def movimenta_via_controle(self,direita,esquerda):
-        teclas = pygame.key.get_pressed()
-        
-        if teclas[direita]:
-            if self.pos_x < 800 - self.largura:
-                self.pos_x = self.pos_x + 5
-
-        if teclas[esquerda]:
-            if self.pos_x > 0:
-                self.pos_x = self.pos_x - 5
+    def movimenta(self):
+        self.pos_x = self.pos_x - self.velocidade
+        if self.pos_x < -200:
+            self.pos_x = 800
+            self.velocidade = random.randint(5,10)
